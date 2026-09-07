015f589888b8be72d2f235bbb3873c96d7b4df0a
DennisFriedelMNP <Dennis.Friedel@med.uni-heidelberg.de>
2026-09-07 14:09:28 +0200

    adjusted for running on other machine

--- R/1_WP_PRC_DEA_20260817_RR1296_PCF.R
+++ R/1_WP_PRC_DEA_20260817_RR1296_PCF.R
@@ -15,8 +15,18 @@ source("./R/utils/utils_module.R")
 source("./R/utils/import_module.R")
 source("./R/utils/run_pathview_in.R")
 
+create_rip_envir(rip_dir = "./")
+
 #### Import data #####
 diann_result_path<-"/mnt/add50/PATHO-PROTEOMICS/bioinformatics/results/DIA/20260817_RR1296_PCF/diann_2.3.1/Lib_free/"
+
+#### Note #####
+# In order to run this code you need to download the Data Stored in PRIDE 
+# For running DIANN analyis with create_diann_se_list 
+# you will need the parquet file and the stats file  
+
+#diann_result_path<-"/mnt/add50/PATHO-PROTEOMICS/bioinformatics/results/DIA/20260817_RR1296_PCF/diann_2.3.1/Lib_free/"
+diann_result_path<-"/data/"
 remove_low_quality <- TRUE
 only_proteotypic <- TRUE
 number_cores = 2
@@ -40,11 +50,12 @@ msdata<-create_diann_se_list(
 )
 saveRDS(msdata,save_here(object_name="diann_list.rds"))
 
-### Annoate
-metadata<-openxlsx::read.xlsx("./projects/20260512_RRS_1296_PCF_Phospho2/sample_sheets/Annotated_sample_sheet3_20260707.xlsx",sheet = 1)
+### Annoate with Supplemental Table 1 
+metadata<-openxlsx::read.xlsx("./Supplemental_table1.xlsx",sheet = 1)
 metadata$original_id<-gsub("_p_raw","",metadata$original_id)
 rownames(metadata)<-metadata$sample_id
 
+### Use Intensities of Proteins mapped to Genes 
 ms_se<-msdata$ggu_se
 ms_se$sample_id<-plyr::mapvalues(colnames(ms_se),metadata$original_id,metadata$sample_id)
 new_cd<-plyr::join(as.data.frame(colData(ms_se)),metadata,by = "sample_id")%>%DataFrame()
@@ -56,83 +67,70 @@ colData(ms_se)<-new_cd
 ms_se_flt<-ms_se[,ms_se$group%in%c("WT","Amp")]
 
 ### Run Preproc and DEA
-tt_comparison<-purrr::map(c(0.8,0.75),function(flt){
-  
-  ### Preprocess
-  ms_se_prc <- proteoLab::pre_processing_wrapper(
-    se_object = ms_se_flt,
-    group_sel = "group",
-    filter_fractioned = T,
-    thr_sample = 5000,
-    thr_feature = flt,
-    normalization_method = "median_center",
-    imputation_method  = "MinProb"
-  )
-  
-  saveRDS(ms_se_prc,save_here(object_name = "ms_se_prc.rds"))
-  
-  qc_plots<-proteoLab::qp_plots_wrapper(se_proc_ls = ms_se_prc,
+### Preprocess
+ms_se_prc <- proteoLab::pre_processing_wrapper(
+  se_object = ms_se_flt,
+  group_sel = "group",
+  filter_fractioned = T,
+  thr_sample = 5000,
+  thr_feature = 0.75,
+  normalization_method = "median_center",
+  imputation_method  = "MinProb"
+)
+
+saveRDS(ms_se_prc, save_here(object_name = "ms_se_prc.rds"))
+
+qc_plots <- proteoLab::qp_plots_wrapper(se_proc_ls = ms_se_prc,
                                         thr_sample = 4000,
                                         group_sel = "group")
-  qc_plots
-  
-  purrr::map2(qc_plots, names(qc_plots),function(x,y){
-    ggplot2::ggsave(
-      filename = save_here(object_name = paste0((y),"_qc_plot.pdf"),analysis = analysis),
-      plot = x,
-      width = 10,
-      height = 10,
-      units = "in",
-      dpi = 300
-    )
-  })
-  
-  ### Differential Expression
-  ms_se_imp<-ms_se_prc$imp
-  ms_se_imp$group<-toupper(ms_se_imp$group)
-  
-  dea_res <- proteoLab::wrapper_dea_gsea(
-    ms_se = ms_se_imp,
-    wrp_group = "group",
-    wrp_test = "AMP",
-    wrp_contrast = list(c("WT")),
-    wrp_mode = "MANUAL",
-    wrp_pv_fil = 0.05,
-    wrp_fc_fil = 0.58,
-    wrp_use_padj = F,
-    gene_set_catalouge = NULL
+qc_plots
+
+purrr::map2(qc_plots, names(qc_plots), function(x, y) {
+  ggplot2::ggsave(
+    filename = save_here(
+      object_name = paste0((y), "_qc_plot.pdf"),
+      analysis = analysis
+    ),
+    plot = x,
+    width = 10,
+    height = 10,
+    units = "in",
+    dpi = 300
   )
-  
-  ttresult<-dea_res$tt_combined
-  ttresult[ttresult$AMP_vs_WT_adj_P_Val<0.05,]
-  
-  dea_res$tt_combined<-NULL
-  purrr::map2(dea_res, names(dea_res),function(x,y){
-    ggplot2::ggsave(
-      filename = save_here(object_name = paste0((y),"_dea_plot.pdf"),analysis = analysis),
-      plot = x,
-      width = 10,
-      height = 10,
-      units = "in",
-      dpi = 300
-    )
-  })
-  openxlsx::write.xlsx(ttresult,save_here(object_name = "TopTable_WT_AMP.xlsx"))
-  saveRDS(ms_se_imp,save_here(object_name = "ms_se_imp.rds"))
-  ttresult
 })
-tt80<-tt_comparison[[1]]
-tt75<-tt_comparison[[2]]
 
-dim(tt80);dim(tt75)
+### Differential Expression
+ms_se_imp <- ms_se_prc$imp
+ms_se_imp$group <- toupper(ms_se_imp$group)
 
-overlap_filter<-ggVennDiagram::ggVennDiagram(x = list("75%(3/5)"=tt75$genes[tt75$significant],
-                                                      "80%(4/5)"=tt80$genes[tt80$significant]))+coord_flip()
-ggsave(plot = overlap_filter,save_here(object_name = "Overlap_SigSites_75_vs_90.pdf"),width = 10,height = 10)
+dea_res <- proteoLab::wrapper_dea_gsea(
+  ms_se = ms_se_imp,
+  wrp_group = "group",
+  wrp_test = "AMP",
+  wrp_contrast = list(c("WT")),
+  wrp_mode = "MANUAL",
+  wrp_pv_fil = 0.05,
+  wrp_fc_fil = 0.58,
+  wrp_use_padj = F,
+  gene_set_catalouge = NULL
+)
 
-only_80<-tt80[which(!tt80$genes[tt80$significant]%in%tt75$genes[tt75$significant]),]
-only_75<-tt75[which(!tt75$genes[tt75$significant]%in%tt80$genes[tt80$significant]),]
-intersect_75_90<-tt75[which(tt75$genes[tt75$significant]%in%tt80$genes[tt80$significant]),]
+ttresult <- dea_res$tt_combined
+ttresult[ttresult$AMP_vs_WT_adj_P_Val < 0.05, ]
 
-tt75[c("EGFR","NES","VIM","TP53BP1","PSIP1","MAP2"),]
-
+dea_res$tt_combined <- NULL
+purrr::map2(dea_res, names(dea_res), function(x, y) {
+  ggplot2::ggsave(
+    filename = save_here(
+      object_name = paste0((y), "_dea_plot.pdf"),
+      analysis = analysis
+    ),
+    plot = x,
+    width = 10,
+    height = 10,
+    units = "in",
+    dpi = 300
+  )
+})
+openxlsx::write.xlsx(ttresult, save_here(object_name = "TopTable_WT_AMP.xlsx"))
+saveRDS(ms_se_imp, save_here(object_name = "ms_se_imp.rds"))
