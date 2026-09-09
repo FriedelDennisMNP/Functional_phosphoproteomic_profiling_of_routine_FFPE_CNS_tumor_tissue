93ad7b3001cb6fd96ee3dba65f05be4cd5c94c5d
DennisFriedelMNP <Dennis.Friedel@med.uni-heidelberg.de>
2026-09-09 14:44:25 +0200

    rename

--- R/2_PTM_PRC_ADJ_DEA_RRS_1296_PCF_Phospho.R
+++ R/2_PTM_PRC_ADJ_DEA_RRS_1296_PCF_Phospho.R
@@ -6,8 +6,8 @@
 ####===================================####
 
 dataset  = '20260813_RRS_1296_PCF_Phospho_Protnorm' # - Name of the dataset that is going to be analysed.
-analysis = paste0('PTM_PRC_DEA/',Sys.Date()) # - Name of the analysis e.g Marker Identification
-"PTM_PRC_DEA_20260813_RRS_1296_PCF_Phospho.R"
+analysis = paste0('PTM_PRC_ADJ_DEA/',Sys.Date()) # - Name of the analysis e.g Marker Identification
+
 
 set.seed(2905)
 library("dplyr")
