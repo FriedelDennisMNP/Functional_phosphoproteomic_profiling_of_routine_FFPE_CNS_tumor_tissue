1962efc3fa631974caf40db4b5e03da990b85c73
DennisFriedelMNP <Dennis.Friedel@med.uni-heidelberg.de>
2026-09-11 09:49:40 +0200

    load rds data instead of import

--- R/1_WP_PRC_DEA_20260817_RR1296_PCF.R
+++ R/1_WP_PRC_DEA_20260817_RR1296_PCF.R
@@ -20,54 +20,8 @@ dataset  = '20260817_RR1296_PCF' # - Name of the dataset that is going to be ana
 analysis = paste0('WP_PRC_DEA/',Sys.Date()) # - Name of the analysis e.g Marker Identification
 create_rip_envir(rip_dir = "./")
 
-#### Import data #####
-#### Note #####
-# In order to run this code you need to download the Data Stored in PRIDE 
-# For running DIANN analyis with create_diann_se_list 
-# you will need the parquet file and the stats file  
-
-diann_result_path<-"./data/20260817_RR1296_PCF/diann_2.3.1/Lib_free/"
-remove_low_quality <- TRUE
-only_proteotypic <- TRUE
-number_cores = 2
-verbose = TRUE
-
-msdata <- load_ms_results(ms_result_dir = diann_result_path)
-msdata$result_stats_tsv$file_name<-gsub(".*/","",msdata$result_stats_tsv$file_name)%>%gsub("[.]raw","",.)
-
-msdata<-create_diann_se_list(
-  result_tsv=msdata$result_parquet,
-  stats_tsv=msdata$result_stats_tsv,
-  remove_low_quality = TRUE,
-  use_only_proteotypic = TRUE,
-  threshold_precursor = 0.05,
-  protein_theshold = 0.05,
-  pq_q_threshold = 0.05,
-  gg_q_threshold = 0.05,
-  pg_quantity=c("pg_max_lfq"),#"pg_normalised"
-  gg_quantity=c("genes_max_lfq"),#"genes_normalised"
-  verbose=TRUE
-)
-
-saveRDS(msdata,save_here(object_name="diann_list.rds"))
-
-### Annoate with Supplemental Table 1 
-metadata<-openxlsx::read.xlsx("./data/Sample_annotation.xlsx",sheet = 1)
-metadata$original_id<-gsub("_p_raw","",metadata$original_id)
-rownames(metadata)<-metadata$sample_id
-
-### Use Intensities of Proteins mapped to Genes 
-
-ms_se<-msdata$ggu_se
-ms_se$sample_id<-plyr::mapvalues(colnames(ms_se),metadata$original_id,metadata$sample_id)
-new_cd<-plyr::join(as.data.frame(colData(ms_se)),metadata,by = "sample_id")%>%DataFrame()
-rownames(new_cd)<-new_cd$sample_id
-colnames(ms_se)<-plyr::mapvalues(colnames(ms_se),metadata$original_id,metadata$sample_id)
-colData(ms_se)<-new_cd
-
-### Drop samples which are not amp or WT
-ms_se_flt<-ms_se[,ms_se$group%in%c("WT","Amp")]
-saveRDS(ms_se_flt,file = "./data/wholeproteome_intensities.rds")
+### Load Whole proteome data
+ms_se_flt<-readRDS("./data/wholeproteome_intensities.rds")
 
 ### Run Preproc and DEA
 ### Preprocess
@@ -80,7 +34,6 @@ ms_se_prc <- pre_processing_wrapper(
   normalization_method = "none",
   imputation_method  = "MinProb"
 )
-
 saveRDS(ms_se_prc, save_here(object_name = "ms_se_prc.rds"))
 
 qc_plots <- qp_plots_wrapper(se_proc_ls = ms_se_prc,
